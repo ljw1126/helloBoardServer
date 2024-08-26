c70bd4d592329e35a06ad7e14c30f48afed5b1b3
leejinwoo <leejinwoo1126@gmail.com>
2024-08-26 11:55:02 +0900

    refactor: @LoginCheck에 접근제어자, static 제거

--- src/main/java/hello/board/server/aop/LoginCheck.java
+++ src/main/java/hello/board/server/aop/LoginCheck.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LoginCheck {
-    public static enum UserType {
+    enum UserType {
         USER, ADMIN
     }
 
