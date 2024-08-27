352583eaebc5b21924b8ce6bc8348fc16bccfec1
leejinwoo <leejinwoo1126@gmail.com>
2024-08-27 13:33:16 +0900

    feat: Redis 설정 추가

--- src/main/java/hello/board/server/BoardApplication.java
+++ src/main/java/hello/board/server/BoardApplication.java
@@ -2,12 +2,14 @@ package hello.board.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class BoardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BoardApplication.class, args);
+    }
 
 }
